54fec3bc250d5dc0c2d951c4578afd2440191716
makeupyourmind <nikitosnov@gmail.com>
2019-02-19 15:48:59 +0200

    ad

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -5,7 +5,7 @@ function Main({onSubmit, onClick})//принятие свойства
 {
     return (
         <div className = "glava">
-        <h1>Example site on React + Node.js + Postgresql</h1>
+        <h1>Example site on React + Node.js + PostgreSQL</h1>
           <div className = "main">
             <button type = "submit" id = "signup" className = "mdc-button mdc-button--primary mdc-button--raised" onClick = {onSubmit}>Sign up</button>
             <button type = "submit" id = "logIn" className = "mdc-button mdc-button--primary mdc-button--raised" onClick = {onClick}>Log In</button>
