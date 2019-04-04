3d6641949cd4e714b41713bdf5cefbad66f5a9af
makeupyourmind <nikitosnov@gmail.com>
2019-04-04 22:19:16 +0300

    ad

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 var bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
